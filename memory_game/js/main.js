a69f2ed6de838d98446fe88b5f5235352515fdac
Somjit Pimson <somjitpimson@Somjits-MacBook-Pro.local>
2019-04-15 17:53:17 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,33 @@
 
 var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+//var cardOne = cards[0];
+//var cardTwo = cards[2];
 var cardThree = cards[1];
 var cardFour = cards[3]; 
 var cardsInPlay = [];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
-    if (cards[0] === cards[1]) {
-    alert("It's a match!");
-} else {
-    alert("Sorry, try again.");
+//cardsInPlay.push(cardOne);
+//cardsInPlay.push(cardTwo);
+function checkForMatch () {
+    if (cardsInPlay.length === 2) {
+     if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("It's a match!");
+    } else {
+        console.log("Sorry, try again.");
+        }
     }
-};
+}
+function flipcard (cardId) {
+    cardsInPlay.push(cards[cardId]);
+    //console.log("User flipped " + cardOne);
+    //console.log("User flipped " + cardTwo);
+    console.log("User flipped" + cards[cardId])
+}
+
+flipcard(0);
+flipcard(2);
+checkForMatch();
+
 
 
 
