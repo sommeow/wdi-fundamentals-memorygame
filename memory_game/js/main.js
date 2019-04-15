4491227e99073a2da403f1f5a76905ccbc9083a5
Somjit Pimson <somjitpimson@Somjits-MacBook-Pro.local>
2019-04-15 19:00:18 -0400

    memory game deliv

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,7 +18,7 @@ var cards = [
 {
     rank: "king",
     suit: "diamonds",
-    cardImage: "images/kind-of-diamonds.png"
+    cardImage: "images/king-of-diamonds.png"
 }
 ];
 //var cardOne = cards[0];
@@ -31,23 +31,32 @@ var cardsInPlay = [];
 function checkForMatch () {
     if (cardsInPlay.length === 2) {
      if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("It's a match!");
+        alert("It's a match!");
     } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
         }
     }
 }
-function flipcard (cardId) {
+function flipcard () {
+    var cardId = this.getAttribute('data-id')
     cardsInPlay.push(cards[cardId].rank);
     console.log("User flipped " + cards[cardId].rank)
     console.log(cards[cardId].suit)
     console.log(cards[cardId].cardImage)
+    this.setAttribute("src", cards[cardId].cardImage);
+    checkForMatch();
+}
+function createBoard () {
+    for (var i =0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipcard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
 }
 
-flipcard(0);
-flipcard(2);
-checkForMatch();
-
+createBoard();
 
 
 
